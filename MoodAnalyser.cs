7f38189aa0871aeb3f8d8772a269279a00176d24
Ruthikreddy <ruthikreddy15@gmail.com>
2020-10-12 19:17:34 +0530

    [Ruthik] push for UC1

---
+++ MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserNamespace
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        /// <summary>
+        /// Parameterised Constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMethod()
+        {
+            if (this.message.Contains("sad"))
+                return "Sad";
+            else
+                return "Happy";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoodAnalyserNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodAnalyser analyser = new MoodAnalyser("I am in sad Mood");
+            Console.WriteLine(analyser.AnalyserMethod());
+            Console.ReadLine();
+        }
+    }
+}
